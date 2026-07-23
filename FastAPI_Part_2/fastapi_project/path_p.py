44321118fad829edef38050f19634440dc3e5c22
Naveen Kumar Singh <142706362+Tgnks@users.noreply.github.com>
2026-07-23 23:43:44 +0530

    Add path parameter API and configure .gitignore

---
+++ FastAPI_Part_2/fastapi_project/path_p.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+customer_risk_profile = {
+    101: {"name":"Naveen Singh", "risk":"low", "score":0.12},
+    102: {"name":"Ruchi Singh", "risk":"Medium", "score":0.54},
+    103: {"name":"Chanchal Singh", "risk":"High", "score":0.89},
+}
+
+@app.get("/customer/{customer_id}")
+def get_customer_risk(customer_id:int):
+    if customer_id not in customer_risk_profile:
+        return {"error":f"customer{customer_id} not found"}
+    profile = customer_risk_profile[customer_id]
+
+    return{
+        "customer_id ":customer_id,
+        "name: ": profile["name"],
+        "risk_level":profile["risk"],
+        "score":profile["score"]
+    }
